5a37d13527603501091f66e1381181652c0bcb95
Kiryl Astapenka <astapenko.1304@gmail.com>
2023-09-02 01:36:57 +0200

    feat: add item to header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// import '@cds/core/icon/register.js';
 import { ClarityModule } from '@clr/angular';
 
 import { AppRoutingModule } from './app-routing.module';
